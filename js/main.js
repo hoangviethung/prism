1758b0c8ff881762f96f75e544b747ae98b9e7a9
hoangviethung <hoangviet.hung.009@gmail.com>
2022-03-31 20:19:23 +0700

    add fixed header

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 function setItemLocal(item, value) {
     window.localStorage.setItem(item, value);
-};
+}
 
 function getItemLocal(item) {
     window.localStorage.getItem(item);
-};
+}
 
 function removeItemLocal(item) {
     window.localStorage.removeItem(item);
-};
+}
